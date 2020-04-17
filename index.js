142c86db4e10df5d25b19022274499f2260cfa20
msnmz <snmez.muharrem@gmail.com>
2020-04-17 23:15:45 +0200

    Diff Two Arrays

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function sumAll(arr) {
 }
 
 console.log(sumAll([1, 4]) === 10);
+
+// Diff Two Arrays
+
+function diffArray(arr1, arr2) {
+  return [...arr1.filter(elm => !arr2.includes(elm)), ...arr2.filter(elm => !arr1.includes(elm))];
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]).length === 5);
